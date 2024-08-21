98aaedc2e357848da94ebf470a88617e1c7b550c
Lucca Carneiro Della Pasqua <luccapasqua@gmail.com>
2024-08-21 15:11:17 -0300

    5th app.jsx updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { SignUp } from "./pages/signup/Signup"
 import { Home } from "./pages/home/Home"
 import {Routes, Route, Navigate} from "react-router-dom"
 import { Toaster } from "react-hot-toast"
-import { useAuthContext } from "./context/authContext"
+import { useAuthContext } from "./context/AuthContext.jsx"
 
 function App() {
   const {authUser} = useAuthContext();
