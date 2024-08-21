04c84b47074d7e3372bb05badff47033efbd9e5b
Lucca Carneiro Della Pasqua <luccapasqua@gmail.com>
2024-08-21 15:44:04 -0300

    Update SocketContext.jsx

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("http://localhost:5001", {
+			const socket = io("https://chat-app-yt-sybc.onrender.com", {
 				query: {
 					userId: authUser._id,
 				},
